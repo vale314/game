b5ebf9bb70290c2c9daeabba693a4dcdc4240126
Vale314 <electrovale314@hotmail.com>
2020-01-28 02:52:13 -0600

    1.0

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
