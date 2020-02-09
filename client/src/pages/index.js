eb505585ad6395f62c3268fae4213135829c204f
FrancisXD <francisco.alejandro.90@gmail.com>
2020-02-08 20:36:46 -0600

    modificaciones de index
    Se modifico el index.js de la carpeta pages:
    1. la barra de navegacion se cambio de color y se cambio su estilo de fuente.
    2. se agrego una carrusel de imagenes a la pagina principal
    3. se agrego un pequeño pie de pagina
    4. la pagina principal tiene una musica de fondo para que se sienta un ambiente mas de casino

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-import { Container, Row, Col } from "reactstrap";
+import {Container} from "reactstrap";
+import {Row} from "reactstrap";
+import {Col} from "reactstrap";
+
+import Carrusel from './Componentes/Carrusel';
+
+import musicaCasino from './musica/musicaCasino.mp3';
+
+import './estilos/index.css'
 
 class Index extends React.Component {
   constructor(props) {
@@ -9,13 +17,21 @@ class Index extends React.Component {
 
   render() {
     return (
-      <Container>
-        <Row>
-          <Col>
-            <h1> Hello Wolrd</h1>
-          </Col>
-        </Row>
-      </Container>
+      <React.Fragment>
+        <main>
+          <audio src={musicaCasino} loop autoPlay></audio>
+          <Container>
+            <Carrusel/>
+          </Container>
+        </main>
+        <footer>
+          <p className="piePagina"><i>
+            <a href="" className="enlacePiePagina">Terminos del casino(en proseso)</a> | 
+            <a href="" className="enlacePiePagina"> Soporte(en proseso)</a> | 
+            <a href="" className="enlacePiePagina"> Contacto(en proseso)</a>
+          </i></p>
+        </footer>
+      </React.Fragment> 
     );
   }
 }
