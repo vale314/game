9e087e35f4bf49683f9397baa6069068144e0ac9
Vale314 <electrovale314@hotmail.com>
2020-02-12 17:13:38 -0600

    deployment

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const idToName = require("./utils/idToName");
 
+const path = require("path");
+
 // Connect Database
 connectDB();
 
@@ -15,6 +17,16 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use("/api/user", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
